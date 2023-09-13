6109f1203880db32ef9a2fadadfd0f8b817ae3ba
Aman Vyas <aman03040@gmail.com>
2023-09-13 22:22:20 +0530

    Angular standalone app

---
+++ src/app/app-route.ts
@@ -0,0 +1,8 @@
+import { HomeComponent } from "./home/home.component";
+
+export const APP_ROUTES = [
+    {
+        path: '',
+        component: HomeComponent
+    }
+]
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { APP_ROUTES } from './app/app-route';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(RouterModule.forRoot(APP_ROUTES))
+  ]
+})
